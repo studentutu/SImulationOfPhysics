08e479cd0cd4b433dd9de03b433c9cf9a3822129
Roman Fedorov <fedorov@plavalaguna.ru>
2020-05-08 12:30:37 +0300

    adding tests. working on the calculate move

---
+++ Assets/App/Scripts/Jewels/Jewels/Board.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Board
+{
+    public enum JewelKind
+    {
+        Empty,
+        Red,
+        Orange,
+        Yellow,
+        Green,
+        Blue,
+        Indigo,
+        Violet
+    };
+
+    public enum MoveDirection
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    };
+
+    public struct Move
+    {
+        public int x;
+        public int y;
+        public MoveDirection direction;
+        public Move(int x, int y, MoveDirection direction)
+        {
+            this.x = x;
+            this.y = y;
+            this.direction = direction;
+        }
+
+        public bool IsTheSame(Move moveToBeChecked)
+        {
+            return x == moveToBeChecked.x && y == moveToBeChecked.y && direction == moveToBeChecked.direction;
+        }
+    };
+    private int width;
+    private int height;
+    private JewelKind[][] actualBoard;
+
+    public Board(JewelKind[][] newBoard)
+    {
+        actualBoard = newBoard;
+        height = newBoard.Length;
+        width = newBoard[0].Length;
+    }
+
+    int GetWidth()
+    {
+        return width;
+    }
+    int GetHeight()
+    {
+        return height;
+    }
+
+    JewelKind GetJewel(int x, int y)
+    {
+        return actualBoard[y][x];
+    }
+    void SetJewel(int x, int y, JewelKind kind)
+    {
+        actualBoard[y][x] = kind;
+    }
+
+    //Implement this function
+    // recursion
+    public Move CalculateBestMoveForBoard()
+    {
+
+        return default;
+    }
+
+
+};
---
+++ Assets/App/Scripts/Jewels/JewelsTests/JewelBoardTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using static Board;
+
+namespace Tests
+{
+    public class JewelBoardTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void JewelBoardTestsSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator JewelBoardTestsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+            var actualBoard = new JewelKind[][]
+            {
+                new JewelKind[]{JewelKind.Blue, JewelKind.Green, JewelKind.Blue},
+                new JewelKind[]{JewelKind.Green, JewelKind.Blue, JewelKind.Green},
+                new JewelKind[]{JewelKind.Indigo, JewelKind.Red, JewelKind.Yellow},
+            };
+
+            var newBoard = new Board(actualBoard);
+            var bestMove = new Move(0, 1, MoveDirection.Down);
+            var AlternativebestMove = new Move(1, 1, MoveDirection.Up);
+
+            var Move = newBoard.CalculateBestMoveForBoard();
+            Debug.LogWarning(" Result : " + newBoard.CalculateBestMoveForBoard());
+            bool isEqual = Move.IsTheSame(bestMove) || Move.IsTheSame(AlternativebestMove);
+            Assert.AreEqual(isEqual, false, " Move is bad");
+        }
+    }
+}
